9f144276df6ea7e28fd6e0a73907ce3be3b575ae
Slurpeesh <JeffNFG@mail.ru>
2023-06-28 17:09:39 +0300

    Fixed internet connection issues

--- app/src/main/java/com/example/weathercheck/lang.kt
+++ app/src/main/java/com/example/weathercheck/lang.kt
@@ -13,7 +13,10 @@ val langDict = mapOf(
         "cancel" to "Cancel",
         "permission_required" to "Permission Required",
         "permission_ask" to "Location permission is required to access weather information in your location. Please enable the location permission in the app settings.",
-        "permission_alert" to "Location permission is denied. You can enable the location permission in the app settings."
+        "permission_alert" to "Location permission is denied. You can enable the location permission in the app settings.",
+        "internet_required" to "No Internet connection",
+        "internet_ask" to "An Internet connection is required to use this application. Please check your connection and try again.",
+        "no_wifi_settings" to "Network settings are not available on your device"
     ),
     "ru" to mapOf(
         "update" to "Обновить",
@@ -27,6 +30,9 @@ val langDict = mapOf(
         "cancel" to "Отмена",
         "permission_required" to "Требуется разрешение",
         "permission_ask" to "Для доступа к информации о погоде в вашем местоположении требуется разрешение местоположения. Включите разрешение на определение местоположения в настройках приложения.",
-        "permission_alert" to "Разрешение на определение местоположения запрещено. Разрешение на определение местоположения можно включить в настройках приложения."
+        "permission_alert" to "Разрешение на определение местоположения запрещено. Разрешение на определение местоположения можно включить в настройках приложения.",
+        "internet_required" to "Отсутствует интернет-соединение",
+        "internet_ask" to "Для использования этого приложения требуется подключение к интернету. Пожалуйста, проверьте ваше подключение и попробуйте снова.",
+        "no_wifi_settings" to "Настройки сети недоступны на вашем устройстве"
     )
 )
